a3b287d91bb13718a969a9328a54d77174b2bcc6
Johnny Wu <KarasAya@gmail.com>
2015-06-17 18:06:50 +0800

    first submit

---
+++ main.js
@@ -0,0 +1,11 @@
+module.exports = {
+    load: function () {
+    },
+
+    unload: function () {
+    },
+
+    'inspector:open': function () {
+        Editor.Panel.open('inspector.panel');
+    },
+};
---
+++ panel/inspector.js
@@ -0,0 +1,14 @@
+(function () {
+var Util = require('util');
+
+Editor.registerPanel( 'inspector.panel', {
+    is: 'editor-inspector',
+
+    properties: {
+    },
+
+    ready: function () {
+    },
+});
+
+})();
