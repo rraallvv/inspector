e4e6d2f3f53d7c01d3ac37830f4e8490a759a7d1
Johnny Wu <KarasAya@gmail.com>
2015-06-23 00:55:24 +0800

    simple redirect inspector

--- panel/inspector.js
+++ panel/inspector.js
@@ -2,6 +2,8 @@
 var Fs = require('fire-fs');
 var Path = require('fire-path');
 
+var _url2imported = {};
+
 Editor.registerPanel( 'inspector.panel', {
     is: 'editor-inspector',
 
@@ -13,6 +15,38 @@ Editor.registerPanel( 'inspector.panel', {
     },
 
     inspect: function ( type, obj ) {
+        this._loadInspector ( type, function ( err, element ) {
+            if ( element ) {
+                element.target = obj;
+
+                var contentDOM = Polymer.dom(this.$.content);
+                contentDOM.removeChild( contentDOM.firstChild );
+                contentDOM.appendChild(element);
+            }
+        }.bind(this));
+    },
+
+    _loadInspector: function ( type, cb ) {
+        var url = Editor.inspector[type];
+        if ( url === undefined ) {
+            if ( cb ) cb ();
+            return;
+        }
+
+        if ( _url2imported[url] ) {
+            var el = document.createElement( type + '-inspector');
+            if ( cb ) cb ( null, el );
+            return;
+        }
+
+        Polymer.Base.importHref( url, function ( event ) {
+            _url2imported[url] = true;
+            var el = document.createElement( type + '-inspector');
+            if ( cb ) cb ( null, el );
+            return;
+        }, function ( err ) {
+            Editor.error( 'Failed to load %s. message: %s', url, err.message );
+        });
     },
 
     'selection:activated': function ( type, id ) {
