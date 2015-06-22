be5ff8547b59e957cb0d2c5ad5e36bcd32ef141f
Johnny Wu <KarasAya@gmail.com>
2015-06-22 23:47:09 +0800

    respond to selection:active

--- panel/inspector.js
+++ panel/inspector.js
@@ -1,5 +1,6 @@
 (function () {
-var Util = require('util');
+var Fs = require('fire-fs');
+var Path = require('fire-path');
 
 Editor.registerPanel( 'inspector.panel', {
     is: 'editor-inspector',
@@ -8,6 +9,30 @@ Editor.registerPanel( 'inspector.panel', {
     },
 
     ready: function () {
+        this.targetName = '';
+    },
+
+    inspect: function ( type, obj ) {
+    },
+
+    'selection:activated': function ( type, id ) {
+        if ( type === 'asset' ) {
+            var fspath = Editor.assetdb.remote._uuid2path[id];
+            if ( fspath ) {
+                this.set('targetName', Path.basenameNoExt(fspath));
+                var metapath = fspath + '.meta';
+                jsonObj = JSON.parse(Fs.readFileSync(metapath));
+
+                var metaType = jsonObj['meta-type'];
+                var metaCtor = Editor.metas[metaType];
+                if ( metaCtor ) {
+                    var meta = new metaCtor();
+                    meta.deserialize(jsonObj);
+
+                    this.inspect( metaType, meta );
+                }
+            }
+        }
     },
 });
 
