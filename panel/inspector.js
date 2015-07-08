47808bdefb08698d10637a4f6f2482e8338c0145
Johnny Wu <KarasAya@gmail.com>
2015-07-08 19:35:04 +0800

    use EditorUI.import

--- panel/inspector.js
+++ panel/inspector.js
@@ -172,15 +172,15 @@ Editor.registerPanel( 'inspector.panel', {
             return;
         }
 
-        Editor._importing = true; // TEMP HACK: please ref scene._popIpc
-        Polymer.Base.importHref( url, function ( event ) {
+        EditorUI.import( url, function ( err ) {
+            if ( err ) {
+                if ( cb ) cb ( err );
+                return;
+            }
+
             _url2imported[url] = true;
             var el = document.createElement( prefix + '-inspector');
-            Editor._importing = false; // TEMP HACK: please ref scene._popIpc
             if ( cb ) cb ( null, el );
-        }, function ( err ) {
-            Editor._importing = false; // TEMP HACK: please ref scene._popIpc
-            if ( cb ) cb ( err );
         });
     },
 
